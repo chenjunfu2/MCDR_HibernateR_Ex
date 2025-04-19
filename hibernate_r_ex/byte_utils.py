b6d5b65e5b7065322498428df8872301268aca1d
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-04-19 08:29:57 +0800

    Revert "修复readvarint"
    This reverts commit 39c2e37deff8fac7fdf2aff031778ad265b92633.

--- hibernate_r_ex/byte_utils.py
+++ hibernate_r_ex/byte_utils.py
@@ -25,11 +25,13 @@ def read_exactly(sock, n, timeout=5):
 
 def read_varint(byte, i):
     result = 0
-    for i in range(6):
-        if i >= 5:#i在0~4共5个索引内，一共能读出5*7=35个bits，刚好大于32，如果再多则varint出错，抛出异常
-            raise IOError("Packet is too long!")
+    bytes_ = 0
+    while True:
         byte_in = byte[i]
-        result |= (byte_in & 0x7F) << (i * 7)
+        i += 1
+        result |= (byte_in & 0x7F) << (bytes_ * 7)
+        if bytes_ > 32:
+            raise IOError("Packet is too long!")
         if (byte_in & 0x80) != 0x80:
             return result, i
 
